24ee510272299fad10aab601c193c55df70cc38a
prestar5 <maxnel2005@gmail.com>
2024-10-01 23:08:14 +0100

    rofi font change and add fzf to install scri[t

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ cd ..
 rm -rf paru
 
 # define packages to install via pacman and paru
-pkg_pacman=("kitty zsh hyprland hyprpaper hyprlock rofi-wayland waybar sddm xdg-desktop-portal-hyprland xdg-desktop-portal nemo nemo-fileroller nemo-image-converter swaync pavucontrol fastfetch btop nwg-look mate-polkit ttf-terminus-nerd ttf-arimo-nerd wl-clipboard grim slurp brightnessctl playerctl noto-fonts-cjk noto-fonts-emoji qt5ct qt6ct papirus-icon-theme zoxide")
+pkg_pacman=("kitty zsh hyprland hyprpaper hyprlock rofi-wayland waybar sddm xdg-desktop-portal-hyprland xdg-desktop-portal nemo nemo-fileroller nemo-image-converter swaync pavucontrol fastfetch btop nwg-look mate-polkit ttf-terminus-nerd ttf-arimo-nerd wl-clipboard grim slurp brightnessctl playerctl noto-fonts-cjk noto-fonts-emoji qt5ct qt6ct papirus-icon-theme zoxide fzf")
 pkg_aur=("hyprpicker hyprshade hyprshot wl-screenrec clipse checkupdates-with-aur auto-cpufreq ungoogled-chromium-bin catppuccin-cursors-mocha rofi-emoji-git")
 
 clear
